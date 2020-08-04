a5d18e3ff1ea76e768384ac0324d3bcd4210bb43
bernethe <bernethe@bernethe.local>
2020-08-04 10:44:53 -0600

    Template Done

--- ri.js
+++ ri.js
@@ -1,1 +1,19 @@
-'use strict';var main=function(){document.getElementById("menu_btn")&&document.getElementById("menu_btn").addEventListener("click",function(){document.getElementById("menu_btn").classList.toggle("opened"),document.querySelector("header.main-header").classList.toggle("opened")},!1);var a=document.body.dataset.page;window[a]&&"function"==typeof window[a]&&window[a]()};document.addEventListener("DOMContentLoaded",main,!1);
+'use strict'; //npm install --save-dev @babel/core @babel/cli @babel/preset-env
+
+var main = function main() {
+  document.getElementById('menu_btn').addEventListener('click', toggleMenu, !1);
+  var _cp = document.body.dataset.page;
+
+  if (window[_cp] && typeof window[_cp] === 'function') {
+    window[_cp]();
+  }
+};
+
+var toggleMenu = function toggleMenu() {
+  document.getElementById('menu_btn').classList.toggle('opened');
+  document.querySelector('header.main-header').classList.toggle('opened');
+};
+
+document.addEventListener('DOMContentLoaded', main, !1);
+
+//# sourceMappingURL=ri.js.map
--- src/app.js
+++ src/app.js
@@ -2,16 +2,14 @@
 //npm install --save-dev @babel/core @babel/cli @babel/preset-env
 
 const main = () => {
-	if(document.getElementById('menu_btn')) {
-		document.getElementById('menu_btn').addEventListener('click', () => {
-			document.getElementById('menu_btn').classList.toggle('opened');
-			document.querySelector('header.main-header').classList.toggle('opened');
-		}, !1);
-	}
+	document.getElementById('menu_btn').addEventListener('click', toggleMenu, !1);
 	let _cp = document.body.dataset.page;
 	if (window[_cp] && typeof window[_cp] === 'function') {
 		window[_cp]();
 	}
 };
-
+const toggleMenu = () => {
+	document.getElementById('menu_btn').classList.toggle('opened');
+	document.querySelector('header.main-header').classList.toggle('opened');
+};
 document.addEventListener('DOMContentLoaded', main, !1);
