705c0b692212e336e12232d88479039478d8ddf0
bernethe <bernethe@bernethe.local>
2020-08-06 21:02:40 -0600

    adelantop valid form

---
+++ src/_functions.js
@@ -0,0 +1,72 @@
+// let docid_rx = new RegExp('^[0-9]{9,12}$');
+// let phone_rx = new RegExp('^[0-9]{8}$');
+let email_rx = new RegExp('^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$');
+const addEventListenerList = (list, event, fn) => {
+	for (var i = 0, len = list.length; i < len; i++) {
+		list[i].addEventListener(event, fn, !1);
+	}
+};
+// const appendPreloader = (pl_txt) => {
+// 	let pldiv = createCustomElement('div',{},[
+// 		createCustomElement('div',{'class':'spinner_mc'}),
+// 		createCustomElement('div',{'class':'status_sp_txt'},[pl_txt])
+// 	]);
+// 	let overall = createCustomElement('div',{'class':'modal-pl'},[pldiv]);
+// 	document.body.appendChild(overall);
+// };
+// const removePreloader = (eRP) => {
+// 	if(eRP) {
+// 		eRP.preventDefault();
+// 	}
+// 	document.body.removeChild(document.querySelector('.modal-pl'));
+// };
+const appendModal = (modalTxt = '', modalTittle, modalFooter) => {
+	let modCont = new Array();
+	if(modalTittle != undefined) {
+		modCont.push( createCustomElement('div',{'class':'modal_header'},[ createCustomElement('h3',{},[modalTittle]) ]) );
+	}
+	modCont.push( createCustomElement('div',{'class':'modal_body'},[modalTxt]) );
+	if(modalFooter != undefined) {
+		modCont.push( createCustomElement('div',{'class':'modal_footer'},[modalFooter]) );
+	}
+	modCont.push( createCustomElement('a',{'href':'#','class':'modal_close'},['×']) );
+	let mdl = createCustomElement('div',{'class':'modalAll'},modCont);
+	let blk = createCustomElement('div',{'id':'modalDialog'},[mdl]);
+	blk.addEventListener('click', e => {
+		if(e.target === blk) {
+			removeModal();
+		}
+	});
+	document.body.appendChild(blk);
+	document.querySelector('.modal_close').addEventListener('click',removeModal);
+};
+const removeModal = (eRM) => {
+	if(eRM) {
+		eRM.preventDefault();
+	}
+	document.body.removeChild(document.getElementById('modalDialog'));
+};
+// https://github.com/escueladigital/EDui
+const createCustomElement = (element,attributes={},children=[]) => {
+	let customElement = document.createElement(element);
+	if (children !== undefined) {
+		children.forEach(el => {
+			if (el.nodeType !== undefined) {
+				if (el.nodeType === 1 || el.nodeType === 11) {
+					customElement.appendChild(el);
+				}
+			} else {
+				customElement.innerHTML += el;
+			}
+		});
+	}
+	addAttributes(customElement,attributes);
+	return customElement;
+};
+const addAttributes = (element, attrObj) => {
+	for (var attr in attrObj) {
+		if (attrObj.hasOwnProperty(attr)) {
+			element.setAttribute(attr,attrObj[attr]);
+		}
+	}
+};
--- src/app.js
+++ src/app.js
@@ -1,16 +1,37 @@
 'use strict';
 //npm install --save-dev @babel/core @babel/cli @babel/preset-env
 
+const toggleMenu = () => {
+	document.getElementById('menu_btn').classList.toggle('opened');
+	document.querySelector('header.main-header').classList.toggle('opened');
+};
+const validateMainForm = (e) => {
+	e.preventDefault();
+	let _m = [];
+	if(document.getElementById('name_txt').value.length < 2) {
+		_m.push( createCustomElement('li',{},['El nombre es requerido']) );
+	}
+	if( !email_rx.test(document.getElementById('email_txt').value)) {
+		_m.push( createCustomElement('li',{},['El correo es requerido y debe ser válido']) );
+	}
+	if(document.getElementById('phone_txt').value.length < 6) {
+		_m.push( createCustomElement('li',{},['El teléfono es requerido']) );
+	}
+
+	if(_m.length != 0) {
+		appendModal( createCustomElement('ul',{},_m), 'Atención' )
+	}
+};
+
 const main = () => {
 	document.getElementById('menu_btn').addEventListener('click', toggleMenu, !1);
 	let _cp = document.body.dataset.page;
 	if (window[_cp] && typeof window[_cp] === 'function') {
 		window[_cp]();
 	}
+	if(document.getElementById('mainForm')) {
+		document.getElementById('mainForm').addEventListener('submit', validateMainForm, !1);
+	}
 	AOS.init();
-};
-const toggleMenu = () => {
-	document.getElementById('menu_btn').classList.toggle('opened');
-	document.querySelector('header.main-header').classList.toggle('opened');
 };
 document.addEventListener('DOMContentLoaded', main, !1);
