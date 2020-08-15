8586f8cb4c713ef45c645a786c09e0994cc5da2a
bernethe <bernethe@bernethe.local>
2020-08-14 19:16:41 -0600

    Validación Formulario Multilingüe

--- src/_validate-form.js
+++ src/_validate-form.js
@@ -2,23 +2,51 @@ const validateMainForm = (e) => {
 	e.preventDefault();
 	let _m = [];
 	if(document.getElementById('name_txt').value.length < 2) {
-		_m.push( createCustomElement('li',{},['El nombre es requerido']) );
+		_m.push( createCustomElement('li',{},[
+			msjContactLang({
+				es:'El nombre es requerido',
+				en:'Name is required'
+			})
+		]) );
 	}
 	if( !email_rx.test(document.getElementById('email_txt').value)) {
-		_m.push( createCustomElement('li',{},['El correo es requerido y debe ser válido']) );
+		_m.push( createCustomElement('li',{},[
+			msjContactLang({
+				es:'El correo es requerido y debe ser válido',
+				en:'Email is required and most be valid'
+			})
+		]) );
 	}
 	if(document.getElementById('phone_txt').value.length < 6) {
-		_m.push( createCustomElement('li',{},['El teléfono es requerido']) );
+		_m.push( createCustomElement('li',{},[
+			msjContactLang({
+				es:'El teléfono es requerido',
+				en:'Phone is required'
+			})
+		]) );
 	}
 	if(document.getElementById('service_txt').value.length < 2) {
-		_m.push( createCustomElement('li',{},['El servicio es requerido']) );
+		_m.push( createCustomElement('li',{},[
+			msjContactLang({
+				es:'El servicio es requerido',
+				en:'Service is required'
+			})
+		]) );
 	}
 	if(document.getElementById('msj_txt').value.length < 2) {
-		_m.push( createCustomElement('li',{},['El mensaje o comentario es requerido']) );
+		_m.push( createCustomElement('li',{},[
+			msjContactLang({
+				es:'El mensaje o comentario es requerido',
+				en:'Message or comments is required'
+			})
+		]) );
 	}
 
 	if(_m.length != 0) {
-		appendModal( createCustomElement('ul',{},_m), 'Atención' );
+		appendModal( createCustomElement('ul',{},_m), msjContactLang({
+			es:'Atención',
+			en:'Warning'
+		}) );
 	} else {
 		let _d = new FormData();
 		_d.append( 'name_txt', document.getElementById('name_txt').value );
@@ -33,4 +61,7 @@ const validateMainForm = (e) => {
 		.then((res) => res.json() )
 		.then((data) => { console.log( data ) });
 	}
+};
+const msjContactLang = (obj) => {
+	return obj[lang];
 };
